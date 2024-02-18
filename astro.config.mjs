3f9a1305811e305c9abf77b07ed75c849bbed860
WGB5445 <919603023@qq.com>
2024-02-18 09:23:38 -0800

    feat: fix dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import solid from '@astrojs/solid-js';
+import solid from '@astrojs/solid-js'; 
 import mdx from "@astrojs/mdx";
 import remarkToc from 'remark-toc';
 import rehypeSlug from 'rehype-slug';
--- src/store/counter.ts
+++
@@ -1,3 +0,0 @@
-// import { atom } from 'nanostores';
-
-// export const counterStore = atom(0);
