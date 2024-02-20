d6162db9a7fed32d371a4990fcd3309a63a851b5
WGB5445 <919603023@qq.com>
2024-02-20 06:54:10 -0800

    fix: formatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { z, defineCollection, type CollectionKey } from "astro:content";
 const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string()),
-      index: z.bigint()
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+    index: z.bigint(),
+  }),
 });
 export async function getCollection(collection: CollectionKey) {
   return getCollection(collection);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,63 +1,60 @@
-import typography from "@tailwindcss/typography"
+import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
-export default ({
+export default {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx,astro}',
-    './node_modules/flowbite/**/*.js'
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx,astro}",
+    "./node_modules/flowbite/**/*.js",
   ],
-  theme:{
+  theme: {
     extend: {
       typography: ({ theme }) => ({
         pink: {
           css: {
-            '--tw-prose-body': theme('colors.pink[800]'),
-            '--tw-prose-headings': theme('colors.pink[900]'),
-            '--tw-prose-lead': theme('colors.pink[700]'),
-            '--tw-prose-links': theme('colors.pink[900]'),
-            '--tw-prose-bold': theme('colors.pink[900]'),
-            '--tw-prose-counters': theme('colors.pink[600]'),
-            '--tw-prose-bullets': theme('colors.pink[400]'),
-            '--tw-prose-hr': theme('colors.pink[300]'),
-            '--tw-prose-quotes': theme('colors.pink[900]'),
-            '--tw-prose-quote-borders': theme('colors.pink[300]'),
-            '--tw-prose-captions': theme('colors.pink[700]'),
-            '--tw-prose-code': theme('colors.pink[900]'),
-            '--tw-prose-pre-code': theme('colors.pink[100]'),
-            '--tw-prose-pre-bg': theme('colors.pink[900]'),
-            '--tw-prose-th-borders': theme('colors.pink[300]'),
-            '--tw-prose-td-borders': theme('colors.pink[200]'),
-            '--tw-prose-invert-body': theme('colors.pink[200]'),
-            '--tw-prose-invert-headings': theme('colors.white'),
-            '--tw-prose-invert-lead': theme('colors.pink[300]'),
-            '--tw-prose-invert-links': theme('colors.white'),
-            '--tw-prose-invert-bold': theme('colors.white'),
-            '--tw-prose-invert-counters': theme('colors.pink[400]'),
-            '--tw-prose-invert-bullets': theme('colors.pink[600]'),
-            '--tw-prose-invert-hr': theme('colors.pink[700]'),
-            '--tw-prose-invert-quotes': theme('colors.pink[100]'),
-            '--tw-prose-invert-quote-borders': theme('colors.pink[700]'),
-            '--tw-prose-invert-captions': theme('colors.pink[400]'),
-            '--tw-prose-invert-code': theme('colors.white'),
-            '--tw-prose-invert-pre-code': theme('colors.pink[300]'),
-            '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
-            '--tw-prose-invert-th-borders': theme('colors.pink[600]'),
-            '--tw-prose-invert-td-borders': theme('colors.pink[700]'),
+            "--tw-prose-body": theme("colors.pink[800]"),
+            "--tw-prose-headings": theme("colors.pink[900]"),
+            "--tw-prose-lead": theme("colors.pink[700]"),
+            "--tw-prose-links": theme("colors.pink[900]"),
+            "--tw-prose-bold": theme("colors.pink[900]"),
+            "--tw-prose-counters": theme("colors.pink[600]"),
+            "--tw-prose-bullets": theme("colors.pink[400]"),
+            "--tw-prose-hr": theme("colors.pink[300]"),
+            "--tw-prose-quotes": theme("colors.pink[900]"),
+            "--tw-prose-quote-borders": theme("colors.pink[300]"),
+            "--tw-prose-captions": theme("colors.pink[700]"),
+            "--tw-prose-code": theme("colors.pink[900]"),
+            "--tw-prose-pre-code": theme("colors.pink[100]"),
+            "--tw-prose-pre-bg": theme("colors.pink[900]"),
+            "--tw-prose-th-borders": theme("colors.pink[300]"),
+            "--tw-prose-td-borders": theme("colors.pink[200]"),
+            "--tw-prose-invert-body": theme("colors.pink[200]"),
+            "--tw-prose-invert-headings": theme("colors.white"),
+            "--tw-prose-invert-lead": theme("colors.pink[300]"),
+            "--tw-prose-invert-links": theme("colors.white"),
+            "--tw-prose-invert-bold": theme("colors.white"),
+            "--tw-prose-invert-counters": theme("colors.pink[400]"),
+            "--tw-prose-invert-bullets": theme("colors.pink[600]"),
+            "--tw-prose-invert-hr": theme("colors.pink[700]"),
+            "--tw-prose-invert-quotes": theme("colors.pink[100]"),
+            "--tw-prose-invert-quote-borders": theme("colors.pink[700]"),
+            "--tw-prose-invert-captions": theme("colors.pink[400]"),
+            "--tw-prose-invert-code": theme("colors.white"),
+            "--tw-prose-invert-pre-code": theme("colors.pink[300]"),
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": theme("colors.pink[600]"),
+            "--tw-prose-invert-td-borders": theme("colors.pink[700]"),
           },
         },
       }),
-      colors:{
+      colors: {
         "jet-black": "#181818",
         "pale-silver": "#f2f3f5",
-      }
+      },
     },
   },
   prefix: "",
-  plugins: [
-    typography()
-  ]
-})
- 
+  plugins: [typography()],
+};
